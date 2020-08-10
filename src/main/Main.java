d50d26557515648eafebf4a9b1ce7491b83a6494
zuo <zuo_hang97@163.com>
2020-08-10 18:25:39 +0800

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: hang hang
+ * @Date: 2020/08/10/18:19
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
